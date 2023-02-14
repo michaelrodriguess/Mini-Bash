8c9cc9f3f9b4c1f9de927dc66b9a9d7a673b0cad
Fernanda Caetano de mattos bastos cunha <fcaetano@c2t1s5.42.rio>
2023-02-14 14:41:34 -0300

    main norminette error fixed

--- sources/main.c
+++ sources/main.c
@@ -6,15 +6,13 @@
 /*   By: microdri <microdri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 17:51:58 by microdri          #+#    #+#             */
-/*   Updated: 2023/02/13 17:53:32 by microdri         ###   ########.fr       */
+/*   Updated: 2023/02/14 14:40:45 by fcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int main()
+int	main(void)
 {
-
 	printf("Entrou\n");
-
 }
