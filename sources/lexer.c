1f3870ba1c6fe062e1f9375c1496dcc5fd2c3630
Fernanda Caetano <fernandacunha@id.uff.br>
2023-02-24 16:59:08 -0300

    save:LexerWithoutLoopNLNE

--- sources/lexer.c
+++ sources/lexer.c
@@ -2,7 +2,22 @@
 
 #include "../includes/minishell.h"
 
-t_token	cat_word( .... )
+t_token	*cat_word(char **c_line)
+{
+	t_token	*current;
+	// char	*str;
+	int		sz;
+
+	sz = 0;
+	current = malloc(sizeof(t_token));
+	while (!ft_strchri(" |><><\"\'$-", (*c_line)[sz]) && (*c_line)[sz] != ' ')
+		sz++;
+	current->type = 0;
+	current->str = ft_substr(*c_line, 0, sz);
+	current->next = NULL;
+	(*c_line)+= sz;
+	return(current);
+}
 
 t_token	*special_chr(int type, char **c_line)
 {
@@ -15,10 +30,10 @@ t_token	*special_chr(int type, char **c_line)
 	{
 		if (ft_strchri(" |><><\"\'$-", (*c_line)[1]) == type)
 		{
-			(*c_line)++;
 			type += 2;
 			free(str);
 			str = ft_substr(*c_line, 0, 2);
+			(*c_line)++;
 		}
 	}
 	current->type = type;
@@ -37,10 +52,14 @@ t_token	*lexer(char* c_line)
 	type = ft_strchri(" |><><\"\'$-", *c_line);
 	if (type)
 		current = special_chr(type, &c_line);
-	else
+	else if ((*c_line) != ' ')
 		current = cat_word(&c_line);
 	if (current)
-		ft_printf("%i\n", ft_strchri(" |><><\"\'$-", *c_line));
+	{
+		ft_printf("%i\n", current->type);
+		ft_printf("%s$\n", current->str);
+	}
+	ft_printf("%s\n", c_line);
 	return (current);
 }
 
