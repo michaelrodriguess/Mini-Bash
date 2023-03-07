f8c61ade489e604c5d4b98f68bc50e6a92908bf5
Michael Rodrigues <run.michael97@gmail.com>
2023-03-07 20:13:42 -0300

    fix: norminette in libft/ft_size_matriz.c; feat: unset overview

--- sources/built-ins/unset.c
+++ sources/built-ins/unset.c
@@ -1,23 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_size_matriz.c                                   :+:      :+:    :+:   */
+/*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: microdri <microdri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/07 17:18:50 by microdri          #+#    #+#             */
-/*   Updated: 2023/03/07 19:23:08 by microdri         ###   ########.fr       */
+/*   Created: 2023/03/07 19:24:01 by microdri          #+#    #+#             */
+/*   Updated: 2023/03/07 19:29:59 by microdri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../../includes/minishell.h"
 
-int	ft_size_matriz(char **matriz)
+// procurar se a string tem na matriz atual
+// se nao tiver return ;
+// se tiver, criar uma nova matriz sem a string
+// dar free na matriz anterior.
+// retornar isso para o data_shell->copy_env sem a string achada.
+
+void	ft_unset(char **temp_copy_env)
 {
-	int	count;
 
-	count = 0;
-	while (matriz[count] != NULL)
-		count++;
-	return (count);
+
 }
