1b19cc73ebcead7b1ee398921425a1b386245a21
Fernanda Caetano <fernandacunha@id.uff.br>
2023-03-11 11:48:26 -0300

    fix: passing no argument when entering only a non existing env variable. ex.: echo

--- sources/handling/parse_str.c
+++ sources/handling/parse_str.c
@@ -12,120 +12,6 @@
 
 #include "../../includes/minishell.h"
 
-/*void	expand(char **env_var, char **env)
-{
-	char	*temp;
-	int		i;
-	int		len;
-
-	i = 0;
-	temp = ft_substr(env[0], 0, ft_strchri(env[0], '=') - 1);
-	while (ft_strcmp(temp, &((*env_var)[1])) && env[i])
-	{
-		i++;
-		free(temp);
-		if (env[i])
-			temp = ft_substr(env[i], 0, ft_strchri(env[i], '=') - 1);
-	}
-	if (!env[i])
-	{
-		free(env_var[0]);
-		env_var[0] = ft_strdup("");
-	}
-	else
-	{
-		len = ft_strlen(env[i]) - ft_strchri(env[i], '=') + 1;
-		free(env_var[0]);
-		env_var[0] = ft_substr(env[i], ft_strchri(env[i], '='), len);
-	}
-}*/
-
-/*char **split_env_var(char *str)
-{
-	int		i;
-	char	**array;
-
-	array = malloc(sizeof(char *) * 2);
-	i = 1;
-	while (ft_isalnum(str[i]) || str[i] == '_')
-		i++;
-	if (i == (int) (ft_strlen(str) - 1))
-	{
-		array[0] = str;
-		array[1] = NULL;
-	}
-	else
-	{
-		array[0] = ft_substr(str, 1, i);
-		array[1] = ft_substr(str, i, ft_strlen(str) - i);
-	}
-	return (array);	
-}
-
-char	*expand_env_var(t_token *tok_lst, char **env)
-{
-	char	**array;
-	char	*arg;
-
-	//(void) *tok_lst;
-	(void) *env;
-	arg = NULL;
-	array = split_env_var(tok_lst->str);
-	if (array[0] && array[1])
-	{
-		arg = ft_strjoin(array[0], array[1]);
-		free(array[0]);
-		free(array[1]);
-	}
-	else if (array[0])
-		arg = ft_strdup("test");
-	else if (array[1])
-		arg = array[1];
-	free(array);
-	return(arg);
-}
-
-char	*add_env_var(t_token **tok_lst, char **env)
-{
-	char	*arg;
-	char	*temp;
-
-	(void) *env;
-	if (strlen((*tok_lst)->str) == 1)
-	{
-		arg = (*tok_lst)->str;
-		*tok_lst = (*tok_lst)->next;
-		return (arg);
-	}
-	else
-	{
-		temp = (*tok_lst)->str;
-		arg = expand_env_var(*tok_lst, env);
-		(*tok_lst)->str = arg;
-		free(temp);
-		*tok_lst = (*tok_lst)->next;
-		return (arg);
-	}
-}*/
-
-/*char	*double_quote(char	**arg, char *parsed_arg)
-{
-	int		i;
-	char	*ret;
-	char	*temp;
-
-	i = 0;
-	while((*arg)[i + 1] != '\"' && (*arg)[i + 1])
-		i++;
-	temp = ft_substr((*arg), 1, i);
-	ret = ft_strjoin(parsed_arg, temp);
-	free(temp);
-	if ((*arg)[i + 1])
-		i++;
-	(*arg) += i + 1;
-	return (ret);
-}*/
-
 void	expand(char **env_var, char **env)
 {
 	char	*temp;
@@ -134,7 +20,7 @@ void	expand(char **env_var, char **env)
 
 	i = 0;
 	temp = ft_substr(env[0], 0, ft_strchri(env[0], '=') - 1);
-	while (ft_strcmp(temp, &((*env_var)[1])) && env[i])// not pass the $ and simplify the code
+	while (env[i] && ft_strcmp(temp, &((*env_var)[1])))// not pass the $ and simplify the code
 	{
 		i++;
 		free(temp);
@@ -176,7 +62,7 @@ void	expand_str(char **str, char **env)
 			env_var = ft_substr(ret, start_envvar, i - start_envvar);
 			expand(&env_var, env);
 			temp = ret;
-			ret = remove_substr(ret, start_envvar, i);
+			ret = remove_substr(ret, start_envvar, i - 1);
 			free (temp);
 			temp = ret;
 			ret = insert_substr(ret, env_var, start_envvar);
@@ -223,10 +109,18 @@ char	*cat_envvar(char **arg, char *parsed_arg, char **env)
 		i++;
 	env_var = ft_substr(*arg, 0, i);
 	expand_str(&env_var, env);
-	*arg += i + 1;
-	temp = env_var;
-	env_var = ft_strjoin(parsed_arg, env_var);
-	free(temp);
+	*arg += i;
+	if (ft_strlen(env_var) == 0 && ft_strlen(parsed_arg) == 0)
+	{
+		free(env_var);
+		return (NULL);
+	}
+	else if (parsed_arg)
+	{
+		temp = env_var;
+		env_var = ft_strjoin(parsed_arg, env_var);
+		free(temp);
+	}
 	return (env_var);
 }
 
@@ -238,6 +132,8 @@ char	*parse_arg(char *arg, char **env)
 	parsed_arg = ft_strdup("");
 	while (*arg)
 	{
+		if (!parsed_arg)
+			parsed_arg = ft_strdup("");
 		temp = parsed_arg;
 		if (*arg == '\"' || *arg == '\'')
 			parsed_arg = quote(&arg, parsed_arg, env);
