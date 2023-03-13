7e2b45977237a25de14c4f947a936ec6f20f21f3
Fernanda Caetano <fernandacunha@id.uff.br>
2023-03-13 20:44:24 -0300

    fix: flag functioning with multiple flags and n's

--- sources/built-ins/echo.c
+++ sources/built-ins/echo.c
@@ -6,28 +6,48 @@
 /*   By: fcaetano <fernandacunha@id.uff.br>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 11:52:54 by fcaetano          #+#    #+#             */
-/*   Updated: 2023/03/06 11:53:04 by fcaetano         ###   ########.fr       */
+/*   Updated: 2023/03/13 20:41:30 by fcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+int	check_flag(char *flag)
+{
+	int	i;
+
+	i = 1;
+	while (flag[i])
+	{
+		if (flag[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 void	ft_echo(char **args)
 {
 	int	n;
+	int	i;
 
+	if (args == NULL)
+		return ;
 	n = 1;
-	if (!ft_strcmp(*args, "-n"))
+	i = 0;
+	while (args[i] && !ft_strncmp(args[i], "-n", 2))
 	{
+		if (!check_flag(args[i]))
+			break;
 		n = 0;
-		args++;
+		i++;
 	}
-	while (*args)
+	while (args[i])
 	{
-		printf("%s", *args);
-		if (args[1] != NULL)
+		printf("%s", args[i]);
+		if (args[i + 1] != NULL)
 			printf(" ");
-		args++;
+		i++;
 	}
 	if (n)
 		printf("\n");
